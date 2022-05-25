678f62086f7b2d8acec374e04cbc851fd6237415
Ekunnusi Theophilus <ekunnusitheophilus@gmail.com>
2022-05-25 16:21:18 +0100

    prismic.io full implementation

---
+++ .slicemachine/assets/slices/DownloadSection/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/DownloadSection';
+
+export default {
+  title: 'slices/DownloadSection'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"download_section","items":[],"primary":{"description":[{"type":"paragraph","text":"Quis aliquip eiusmod excepteur et voluptate. Magna anim sunt in in reprehenderit reprehenderit ut id qui exercitation. Id id est deserunt ex minim nisi sunt duis in elit eu sint.","spans":[]}],"title":[{"type":"paragraph","text":"Eiusmod incididunt sit laborum consectetur minim irure cillum voluptate.","spans":[]}],"playstore":{"dimensions":{"width":900,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1545239351-1141bd82e8a6?w=900&h=500&fit=crop"},"applestore":{"dimensions":{"width":900,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1587905069134-008460d7a636?w=900&h=500&fit=crop"}},"id":"_Default"}} />
+_Default.storyName = 'Default'
