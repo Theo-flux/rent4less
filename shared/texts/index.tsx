e0c99e4d52bb8d155c040990a793f24779dc9f78
Ekunnusi Theophilus <ekunnusitheophilus@gmail.com>
2022-05-27 11:35:10 +0100

    text size fix

--- shared/texts/index.tsx
+++ shared/texts/index.tsx
@@ -7,7 +7,7 @@ export const Title = ({text, className}: TTitleProps) => {
     return(
         <h1
             className={`
-                text-xl md:text-4xl lg:text-5xl 
+                text-2xl md:text-4xl lg:text-5xl 
                 font-axi text-russian
                 ${className}
             `}
